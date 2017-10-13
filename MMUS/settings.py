7d84418906570a16b86dc08ceca44497bde2e7f8
Tlakatlekutl <ary3004@yandex.ru>
2017-10-13 17:20:11 +0300

    Staticfiles settings

--- MMUS/settings.py
+++ MMUS/settings.py
@@ -121,6 +121,9 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static') 
 
-# MEDIA_URL = '/uploads/'
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+
 MEDIA_ROOT = os.path.join(BASE_DIR, 'datasets')
 MEDIA_URL = '/datasets/'
